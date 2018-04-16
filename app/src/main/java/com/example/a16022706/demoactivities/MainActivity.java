a2648d9e44592d1530763a7691b5f98ab5c1c28e
16022706 <16022706@myrp.edu.sg>
2018-04-16 22:39:18 +0800

    You are hema, age 20

--- app/src/main/java/com/example/a16022706/demoactivities/MainActivity.java
+++ app/src/main/java/com/example/a16022706/demoactivities/MainActivity.java
@@ -1,7 +1,14 @@
 package com.example.a16022706.demoactivities;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +16,23 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Button btnDone =  (Button)findViewById(R.id.buttonDone);
+
+        btnDone.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View arg0) {
+                EditText etName =  (EditText) findViewById(R.id.editTextName);
+                EditText etAge =  (EditText) findViewById(R.id.editTextAge);
+                String[] info = {etName.getText().toString(),etAge.getText().toString()};
+
+                Intent i = new Intent(MainActivity.this,DemoActivities2.class);
+
+                i.putExtra("info",info);
+
+                startActivity(i);
+
+            }
+        });
+
     }
 }
